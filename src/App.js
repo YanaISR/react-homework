ab102637aef9d832dc503c69951be6cf4168726b
Yana <miriel.yana@gmail.com>
2023-09-25 19:09:10 +0300

    'hw-23 'useState' is completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const decrementValue = () => {
+    setCount(count - 1);
+  }
+
+  const reset = () => {
+    setCount(0);
+  }
+
+  const incrementValue = () => {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Counter: {count}</h2>
+      <button onClick={decrementValue}>-</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={incrementValue}>+</button>
     </div>
   );
 }
